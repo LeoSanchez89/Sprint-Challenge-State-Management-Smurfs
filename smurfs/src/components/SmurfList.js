68bf413a664b110efc2622422d7f9a1846baa0eb
LeoSanchez89 <58442719+LeoSanchez89@users.noreply.github.com>
2020-02-21 12:38:18 -0500

    rendering info from API

---
+++ smurfs/src/components/SmurfList.js
@@ -0,0 +1,25 @@
+import React, {useContext} from "react";
+import { SmurfContext } from "../contexts/SmurfContext";
+
+const SmurfList = () => {
+
+    const smurfs  = useContext(SmurfContext);
+    console.log(smurfs);
+    return (
+        
+        <div>
+            {smurfs.map(smurf => {
+                return (
+                    <div key={smurf.id}>
+                        <p>{smurf.name}</p>
+                        <p>Age: {smurf.age} years</p>
+                        <p>Height: {smurf.height}</p>
+                    </div>
+                )
+            })}
+        </div>
+    )
+
+}
+
+export default SmurfList;
